c2323d055a11cff089a1903122b4ddb332bd0ff2
Victor Wu <victor.wu@zattoo.com>
2020-12-16 18:17:28 +0100

    Add channel up/down based on https://github.com/googleads/googleads-ima-html5-dai/tree/master/native/simple

---
+++ dai.js
@@ -0,0 +1,181 @@
+var BACKUP_STREAM =
+    'http://storage.googleapis.com/testtopbox-public/video_content/bbb/' +
+    'master.m3u8';
+
+// Live stream asset key.
+var TEST_ASSET_KEY = 'sN_IYUG8STe1ZzhIIE_ksA';
+
+// VOD content source and video IDs.
+var TEST_CONTENT_SOURCE_ID = '2528370';
+var TEST_VIDEO_ID = 'tears-of-steel';
+
+// StreamManager which will be used to request ad-enabled streams.
+var streamManager;
+
+// hls.js video player
+var hls = new Hls();
+
+// Video element
+var videoElement;
+
+// Ad UI element
+var adUiElement;
+
+let channel = 0;
+
+const channelList = [
+  {
+    name: 'Channel 1. DAI DEMO Stream', 
+    dai: true,
+  },
+  {
+    name: 'Channel 2. HD DEMO Stream', 
+    url: 'https://demo-hls7-live.zahs.tv/hd/master.m3u8',
+  },
+  {
+    name: 'Channel 3. Full HD DEMO Stream', 
+    url: 'https://demo-hls7-live.zahs.tv/fullhd/master.m3u8',
+  },
+];
+
+/**
+ * Initializes the video player.
+ */
+function initPlayer() {
+  videoElement = document.getElementById('video');
+  adUiElement = document.getElementById('adUi');
+  streamManager =
+      new google.ima.dai.api.StreamManager(videoElement, adUiElement);
+  streamManager.addEventListener(
+      [
+        google.ima.dai.api.StreamEvent.Type.LOADED,
+        google.ima.dai.api.StreamEvent.Type.ERROR,
+        google.ima.dai.api.StreamEvent.Type.AD_BREAK_STARTED,
+        google.ima.dai.api.StreamEvent.Type.AD_BREAK_ENDED
+      ],
+      onStreamEvent, false);
+
+  // Add metadata listener. Only used in LIVE streams. Timed metadata
+  // is handled differently by different video players, and the IMA SDK provides
+  // two ways to pass in metadata, StreamManager.processMetadata() and
+  // StreamManager.onTimedMetadata().
+  //
+  // Use StreamManager.onTimedMetadata() if your video player parses
+  // the metadata itself.
+  // Use StreamManager.processMetadata() if your video player provides raw
+  // ID3 tags, as with hls.js.
+  hls.on(Hls.Events.FRAG_PARSING_METADATA, function(event, data) {
+    if (streamManager && data) {
+      // For each ID3 tag in our metadata, we pass in the type - ID3, the
+      // tag data (a byte array), and the presentation timestamp (PTS).
+      data.samples.forEach(function(sample) {
+        streamManager.processMetadata('ID3', sample.data, sample.pts);
+      });
+    }
+  });
+
+  // requestVODStream(TEST_CONTENT_SOURCE_ID, TEST_VIDEO_ID, null);
+  // Uncomment line below and comment one above to request a LIVE stream.
+  requestLiveStream(TEST_ASSET_KEY, null);
+  videoElement.controls = true;
+}
+
+function channelUp() {
+  channel = (channel + 1) % channelList.length;
+  loadStream();
+}
+
+function channelDown() {
+  channel = (channel + 1) % channelList.length;
+  loadStream();
+}
+
+/**
+ * Load current channel
+ */
+function loadStream() {
+  const currentChannel = channelList[channel];
+  document.getElementById('channel').innerHTML = currentChannel.name;
+  if (currentChannel.dai) {
+    requestLiveStream(TEST_ASSET_KEY, null);
+  } else {
+    loadUrl(currentChannel.url);
+  }
+}
+
+/**
+ * Play video (In case it's autoplay block with unmuted video)
+ */
+function play() {
+  hls.media.play();
+}
+
+/**
+ * Requests a Live stream with ads.
+ * @param  {string} assetKey
+ * @param  {?string} apiKey
+ */
+function requestLiveStream(assetKey, apiKey) {
+  var streamRequest = new google.ima.dai.api.LiveStreamRequest();
+  streamRequest.assetKey = assetKey;
+  streamRequest.apiKey = apiKey || '';
+  streamManager.requestStream(streamRequest);
+}
+
+/**
+ * Requests a VOD stream with ads.
+ * @param  {string} cmsId
+ * @param  {string} videoId
+ * @param  {?string} apiKey
+ */
+function requestVODStream(cmsId, videoId, apiKey) {
+  var streamRequest = new google.ima.dai.api.VODStreamRequest();
+  streamRequest.contentSourceId = cmsId;
+  streamRequest.videoId = videoId;
+  streamRequest.apiKey = apiKey;
+  streamManager.requestStream(streamRequest);
+}
+
+/**
+ * Responds to a stream event.
+ * @param  {StreamEvent} e
+ */
+function onStreamEvent(e) {
+  switch (e.type) {
+    case google.ima.dai.api.StreamEvent.Type.LOADED:
+      console.log('Stream loaded');
+      loadUrl(e.getStreamData().url);
+      break;
+    case google.ima.dai.api.StreamEvent.Type.ERROR:
+      console.log('Error loading stream, playing backup stream.' + e);
+      loadUrl(BACKUP_STREAM);
+      break;
+    case google.ima.dai.api.StreamEvent.Type.AD_BREAK_STARTED:
+      console.log('Ad Break Started');
+      videoElement.controls = false;
+      adUiElement.style.display = 'block';
+      break;
+    case google.ima.dai.api.StreamEvent.Type.AD_BREAK_ENDED:
+      console.log('Ad Break Ended');
+      videoElement.controls = true;
+      adUiElement.style.display = 'none';
+      break;
+    default:
+      break;
+  }
+}
+
+/**
+ * Loads and plays a Url.
+ * @param  {string} url
+ */
+function loadUrl(url) {
+  console.log('Loading:' + url);
+  hls.detachMedia();
+  hls.loadSource(url);
+  hls.attachMedia(videoElement);
+  hls.on(Hls.Events.MANIFEST_PARSED, function() {
+    console.log('Video Play');
+    videoElement.play();
+  });
+}
